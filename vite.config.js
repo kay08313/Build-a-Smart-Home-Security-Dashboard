d0d9da63b70e28f4c808485e187e7bede3cb3325
kaychiawoon <kaychiawoon@gmail.com>
2026-09-14 05:25:41 +0900

    fix livepage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Build-a-Smart-Home-Security-Dashboard/", // <-- add this
   plugins: [react()],
-})
+});
